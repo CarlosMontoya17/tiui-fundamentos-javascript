454a303a17cba286fd7db9f091a228666ca98838
Carlos Montoya <98224572+CarlosMontoya17@users.noreply.github.com>
2024-07-02 02:44:51 -0600

    [feature] last challenge TS

---
+++ src/ts/last.ts
@@ -0,0 +1,37 @@
+import { XMLHttpRequest } from 'xmlhttprequest-ts';
+import { ICharacter } from './models/caracter';
+import { ILocation } from './models/location';
+import { IGeneral } from './models/response';
+
+var API = 'https://rickandmortyapi.com/api/character/';
+var xhttp: XMLHttpRequest = new XMLHttpRequest();
+
+function fetchData<T>(url_api: string) {
+  return new Promise<T>((res, rej) => {
+    xhttp.onreadystatechange = () => {
+      if (xhttp.readyState === 4) {
+        if (xhttp.status === 200)
+          res(JSON.parse(xhttp.responseText));
+        else return rej(url_api);
+      }
+    }
+    xhttp.open('GET', url_api, true);
+    xhttp.send();
+  });
+};
+
+
+fetchData<IGeneral>(API)
+.then(async (d) => {
+  console.log('Primer Llamado...');
+  const f = await fetchData<ICharacter>(API+d.results[0].id);
+  console.log('Segundo Llamado...');
+  const h = await fetchData<ILocation>(f.origin.url);
+  console.log('Tercer Llamado...');
+  console.log(`Personajes: ${d.info.count}`);
+  console.log(`Primer Personaje: ${f.name}`);
+  console.log(`Dimensión: ${h.dimension}`);
+})
+.catch(error => {
+  console.log(`Error ${error}`);
+});
