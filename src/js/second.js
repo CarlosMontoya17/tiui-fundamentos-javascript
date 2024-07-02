61605ae565d15bf5e5fddb6bcff95e825aac44ee
Carlos Montoya <98224572+CarlosMontoya17@users.noreply.github.com>
2024-07-02 02:43:28 -0600

    [feature] second challenge

---
+++ src/js/second.js
@@ -0,0 +1,34 @@
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
+var A = 'https://rickandmortyapi.com/api/character/';
+var B = new XMLHttpRequest();
+
+const X = (a, b) => {
+  B.onreadystatechange = (e) => {
+    if (B.readyState == '4') {
+      if (B.status === 200)
+        b(null, JSON.parse(B.responseText));
+      else return b(a);
+    }
+    else return b(a);
+  };
+ 
+  B.open('GET', a, false);
+  B.send();
+};
+
+X(A, (c, d) => {
+  if (c) return console.error(`Error ${c}`);
+  console.log('Primer Llamado...');
+  X(A + d.results[0].id, (e, f) => {
+    if (e) return console.error(`Error ${e}`);
+    console.log('Segundo Llamado...');
+    X(f.origin.url, (g, h) => {
+      if (g) return console.error(`Error ${g}`);
+      console.log('Tercer Llamado...');
+      console.log(`Personajes: ${d.info.count}`);
+      console.log(`Primer Personaje: ${f.name}`);
+      console.log(`Dimensión: ${h.dimension}`);
+    });
+  });
+});
